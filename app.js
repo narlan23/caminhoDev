85e7e5fffcf6a188d6d99a0a213ea65ea212a996
Narlan Mota <narlanmota19@gmail.com>
2024-08-21 04:16:47 -0300

    Primeira melhoria no codigo

--- app.js
+++ app.js
@@ -1,44 +1,64 @@
 const pergunta = document.getElementById('pergunta');
 const btn1 = document.getElementById('btn1');
 const resp = document.getElementById('resp');
+let Oresp = "";
 
 btn1.addEventListener('click',function(){
     pergunta.innerText = "Se quer seguir para área de Front-End ou seguir para a área de Back-End?";
-    const resper = resp.value;
-    if(resper == "Front-End"){
+    Oresp = resp.value;
+    if(Oresp.toLowerCase() === "front-end"){
         pergunta.innerText =  "se quer aprender React ou aprender Vue?";
-        quest01();
-    }else if(resper == "Back-End"){
+        btn1.onclick = function(){
+            quest01();
+        }     
+    }else if(Oresp == "Back-End"){
         pergunta.innerText =  "se quer aprender C# ou aprender Java?";
         quest01();
     }
 });
 
 function quest01(){
-    btn1.addEventListener('click',function(){
-        pergunta.innerText = "Se quer seguir entre se especializando na área escolhida ou seguir se desenvolvendo para se tornar Fullstack.";
-        const resp2 = resp.value;
-        if(resp2 == "Especializar"){
-            //pergunta.innerText =  "se torne um especialista!";
-            loopQuest();
-        }else if(resp2 == "FullStack"){
-            //pergunta.innerText =  "se torne um desenvolverdor fullstack!";
-            loopQuest();
+    
+        pergunta.innerText = "Se quer seguir se especializando na área escolhida ou seguir se desenvolvendo para se tornar Fullstack.";
+        //Oresp = resp.value;
+        if(Oresp.toLowerCase() === "especializar"){
+            pergunta.innerText =  "se torne um especialista!";
+            btn1.onclick = function(){
+                quest02();
+            }
+        }else if(Oresp.toLowerCase() === "fullstack"){
+            pergunta.innerText =  "se torne um desenvolverdor fullstack!";
+            btn1.onclick = function(){
+                quest02();
+            }
         }
-    });
+    
+}
+
+function quest02(){
+        btn1.onclick = function(){
+            if(Oresp.toLowerCase() === "especializar"){
+                pergunta.innerText = "Quais são as tecnologias que deseja especializar ou de conhecer?";
+                loopQuest();
+            }else if(Oresp.toLowerCase() === "fullstack"){
+                pergunta.innerText = "Quais são as tecnologias deseja aprender?"
+                loopQuest();
+            }
+        }     
 }
 
 function loopQuest() {
-    // Define a pergunta no elemento
-    pergunta.innerText = "Quais são as tecnologias nas quais a pessoa gostaria de se especializar ou de conhecer?";
+    
 
     let resp3 = "";
     let text = "";
 
     // Adiciona um evento de clique ao botão
-    btn1.addEventListener('click', function() {
+    
         // Loop para acumular as tecnologias até que o usuário digite "Fim"
         while (resp3.toLowerCase() !== "fim") {
+            // Define a pergunta no elemento
+            pergunta.innerText = "Quais são as tecnologias nas quais a pessoa gostaria de se especializar ou de conhecer?";
             // Pede ao usuário para digitar uma tecnologia
             resp3 = prompt("Digite uma tecnologia (ou 'Fim' para terminar):");
 
@@ -53,5 +73,5 @@ function loopQuest() {
 
         // Exibe as tecnologias acumuladas
         alert("Suas Tecnologias são: " + text);
-    });
+    
 }
